c1212f5b316bcc749adfe9b67b0b29220618cb04
danthamGowthami <76723802+danthamGowthami@users.noreply.github.com>
2021-07-12 19:26:29 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+let btn1=document.querySelector('#btn-1');
+let btn2=document.querySelector('#btn-2');
+let btn3=document.querySelector('#btn-3');
+
+btn1.addEventListener('click',()=>{
+  document.body.style.backgroundImage="url('BTN.jpeg')"
+});
+
+
+btn2.addEventListener('click',()=>{
+  document.body.style.backgroundImage="url('C.jpeg')"
+});
+
+btn3.addEventListener('click',()=>{
+  document.body.style.backgroundImage="url('B.jpeg')"
+});
